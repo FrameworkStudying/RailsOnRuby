501944a9060935d03230384811800ca620580d28
Core <hfreedomx@gmail.com>
2018-04-03 22:15:12 +0900

    [  1. Add User model.  2. Add unit test code about isAdmin? method in User class.  3. Run command : docker-compose run webapp rspec spec/models/user_spec.rb  4. test result : 1 example, 0 failures ]

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
+    # the attr_accessor could add a pair of set/get method for the member variable(member field)
+    # should use comma to separate the member variables
+    attr_accessor :id, :username, :password, :isAdministrator
+
+    def initialize(params)
+        @id = params[:id]
+        @username = params[:username]
+        @password = params[:password]
+        @isAdministrator = params[:isAdministrator]
+    end
+
+    def isAdmin?
+        if @isAdmin == 1
+            return true
+        else
+            return false
+        end
+    end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it "isAdmin? : a normal user" do
+        params = {
+            id: 1,
+            username: 'core',
+            password: '111111',
+            isAdministrator: 0
+        }
+        user = User.new(params)
+        expect(user.isAdmin?).to eq(false)
+    end
 end
