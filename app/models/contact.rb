15f64c80def5d338cd983eb332663b274b13a3eb
Core <hfreedomx@gmail.com>
2018-04-09 21:56:39 +0900

    [  1. Add a example rspec code to check the name method of Contact model.  2. Add name method in Contact model.  3. Test code result : 4 examples, 0 failures ]

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,8 @@
 class Contact < ApplicationRecord
     attr_accessor :firstname, :lastname
     validates :firstname, :lastname, presence: true
+
+    def name
+    	[firstname, lastname].join " "
+    end
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,15 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
     it "has a valid factory" do
+    	# the create approach will build the model and save it
         expect(create(:contact)).to be_valid
     end
 
     it "is invalid without a firstname" do
+    	# the build approach will build the model and do not save it
         expect(build(:contact, firstname: nil)).to_not be_valid
     end
-    
+
     it "is invalid without a lastname" do
         expect(build(:contact, firstname: nil)).to_not be_valid
     end
-    it "return a contact's full name as a string"
+    
+    it "return a contact's full name as a string" do
+        contact = build(:contact, firstname: "Xiang", lastname: "Hou")
+        expect(contact.name).to eq("Xiang Hou")
+    end
 end
