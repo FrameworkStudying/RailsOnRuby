ec62e87176ab92cc63bbb3fe370f36889567d0d1
Core <hfreedomx@gmail.com>
2018-04-09 20:24:16 +0900

    [  1. add test code for Contact model.  2. running result : 4 examples, 0 failures, 4 pending ]

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
-
+    describe "GET index" do
+        context "when render index page" do
+            it "has a 200 status code" do
+                get :index
+                expect(response.status).to eq(200)
+            end
+        end
+    end
 end
